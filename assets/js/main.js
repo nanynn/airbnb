138c9d553ff46912c7c0544a8bcfd2333ece247a
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-08 12:25:49 -0400

    Funcionalidad botón filtros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 	$(".filtros-avanzados").hide();
+	$(".boton-filtros").click(function(event){
+  		event.preventDefault();
+		//$(".filtros-avanzados").toggle("slow","display");
+		$(".filtros-avanzados").show();
+		
+	});
+	$(".boton-filtro-cerrar").click(function(event){
+  		event.preventDefault();
+		$(".filtros-avanzados").hide();
+	});
+
+	
+	
 })
 
--- dist/js/script.js
+++ dist/js/script.js
@@ -20255,6 +20255,19 @@ if (jQuery) {
 
 $(document).ready(function(){
 	$(".filtros-avanzados").hide();
+	$(".boton-filtros").click(function(event){
+  		event.preventDefault();
+		//$(".filtros-avanzados").toggle("slow","display");
+		$(".filtros-avanzados").show();
+		
+	});
+	$(".boton-filtro-cerrar").click(function(event){
+  		event.preventDefault();
+		$(".filtros-avanzados").hide();
+	});
+
+	
+	
 })
 
 
