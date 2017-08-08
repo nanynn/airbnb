acc002faab08048ffaa29c933c2e846c447a6acd
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-08 17:47:37 -0300

    instalación de datepicker plugin de jquery

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var minifyCSS = require('gulp-minify-css');
 var webserver = require('gulp-webserver');
 
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'node_modules/jquery-datepicker/jquery-datepicker.js',  'assets/js/*.js'])
 		.pipe(concat('script.js'))
 		//carpeta dist
 		.pipe(gulp.dest('dist/js/'));
