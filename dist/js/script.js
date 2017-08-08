01056a422333f498f1cb5edc203c51f7729b875a
JosefaHerrera <josefa@linuxero.cl>
2017-08-08 12:45:30 -0400

    arreglando sass

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,6 +20253,20 @@ if (jQuery) {
   };
 })(jQuery);
 
-$(document).ready(function(){
-	alert("hola");
-})
+/*VALIDACIONES*/
+//Al momento de hacer click, se muentran las validaciones
+$(document).on("ready", inicio);
+function inicio(){
+	$("#bFiltro").on("click", validarDatos);
+}
+function validarDatos(){
+	var cb = $("input[type = 'checkbox']:checked");
+	var habitaciones = $("#habitaciones option:selected");
+
+	if (cb.length == 0) {
+		$("#mensaje1").fadeIn();
+		return false;
+	}else{
+		$("#mensaje1").fadeOut();
+	}
+}
