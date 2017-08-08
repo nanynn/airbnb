78ace425f1a27c22f488626a8aab810349144bce
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-08 10:43:16 -0400

    Comenzando mapa

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,3 +20253,26 @@ if (jQuery) {
   };
 })(jQuery);
 
+$(document).ready(function(){
+	
+})
+function initMap(){
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+
+	var map = new google.maps.Map(document.getElementById("map"),{
+	zoom: 4,
+    center: {lat: -33.416667,lng: -70.65},
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ['roadmap', 'terrain']
+    }
+
+	});
+	/*directionsDisplay.setMap(map);
+		var inicio = (document.getElementById('busqueda')); 
+		var autocompletar = new google.maps.places.Autocomplete(inicio);
+		autocompletar.bindTo('bounds', map);          
+*/
+}
