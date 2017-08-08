d45a8c7710a1992c39c911472b6cd719e99bba39
JosefaHerrera <josefa@linuxero.cl>
2017-08-08 11:47:59 -0400

    realizando validaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-$(document).ready(function(){
-	alert("hola");
-})
-=======
->>>>>>> b275cb1b722b01748be36f34dbcfcdccde10f27d
-=======
->>>>>>> c9a275b92424971627cd1af15d90fe3adfeea21b
+/*VALIDACIONES*/
+//Al momento de hacer click, se muentran las validaciones
+$(document).on("ready", inicio);
+function inicio(){
+	$("#bFiltro").on("click", validarDatos);
+}
+function validarDatos(){
+	var cb = $("input[type = 'checkbox']:checked");
+	var habitaciones = $("#habitaciones option:selected");
+
+	if (cb.length == 0) {
+		$("#mensaje1").fadeIn();
+		return false;
+	}else{
+		$("#mensaje1").fadeOut();
+	}
+}
