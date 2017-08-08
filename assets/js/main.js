9819a2f5e3beafd027a9adb225e5f7ec933605ce
romina <romina.torres.b@gmail.com>
2017-08-08 11:43:44 -0400

    detalles resultado

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-$(document).ready(function(){
-	alert("hola");
-})
-=======
->>>>>>> b275cb1b722b01748be36f34dbcfcdccde10f27d
-=======
->>>>>>> c9a275b92424971627cd1af15d90fe3adfeea21b
