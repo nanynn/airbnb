017c34a52a3c8a9ef0abbbafb96b643d644b12c5
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-09 00:59:06 -0300

    intento de local storage para guardar los datos ingresados por el usuario

---
+++ assets/js/resultado.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	var lugarGuardado = $("#donde").text(localStorage.getItem("lugar"));
+	var llegadaGuardado = $("#llegada").text(localStorage.getItem("llegada"));
+	var salidaGuardado = $("#salida").text(localStorage.getItem("salida"));
+	var personasGuardado = $("#cantidad").text(localStorage.getItem("personas"));
+
+	$("#llegadaDos").append(llegadaDos);
+	$("#salidaDos").append(salidaGuardado);
+	
+});
